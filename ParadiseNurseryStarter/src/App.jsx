6ef7a05d3af4dcfc3247a241d5b8213e3c37fe99
Aphelele Martin Bojana <126065736+aphelele-bojana@users.noreply.github.com>
2026-06-14 15:11:34 +0200

    Update App.jsx

--- ParadiseNurseryStarter/src/App.jsx
+++ ParadiseNurseryStarter/src/App.jsx
@@ -1,3 +1,31 @@
-import React from 'react';
-function App(){ return <h1>Paradise Nursery</h1>; }
+import React, { useState } from "react";
+import AboutUs from "./components/AboutUs";
+import ProductList from "./components/ProductList";
+
+function App() {
+  const [page, setPage] = useState("home");
+
+  return (
+    <div>
+      {page === "home" && (
+        <div className="landing">
+          <h1>Paradise Nursery 🌿</h1>
+          <p>Bring nature into your home</p>
+
+          <button onClick={() => setPage("products")}>
+            Get Started
+          </button>
+        </div>
+      )}
+
+      {page === "products" && (
+        <>
+          <AboutUs />
+          <ProductList />
+        </>
+      )}
+    </div>
+  );
+}
+
 export default App;
